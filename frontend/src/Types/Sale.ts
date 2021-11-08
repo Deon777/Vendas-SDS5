ddad04ef3635db02054f1c246e845d53002585cd
Igor Deon <igordeon709@gmail.com>
2021-11-07 21:13:05 -0300

    BarChart integration

--- frontend/src/Types/Sale.ts
+++ frontend/src/Types/Sale.ts
@@ -2,3 +2,9 @@ export type SaleSum = {
     name: string;
     amount: number;
 }
+
+export type SaleSucess = {
+    name: string;
+    visited: number;
+    deals: number;
+}
--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,6 +1,39 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSucess } from 'Types/Sale';
+import { round } from 'Utility/Format';
+import { BASE_URL } from 'Utility/requests';
+
+type SeriesData = {
+  name: string,
+  data: number[]
+}
+
+type DataChart = {
+  labels: {
+    categories: string[];
+  };
+  series: SeriesData[];
+}
 
 function BarChart() {
+
+  const [dataChart, setDataChart] = useState<DataChart>({ labels: { categories: [] }, series: [] });
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/success-by-seller`)
+      .then(res => {
+        const data = res.data as SaleSucess[];
+        const newLabels = data.map(x => x.name);
+        const newSeries = data.map(x => round((100.0 * x.deals / x.visited), 1));
+        setDataChart({
+          labels: {categories: newLabels},
+          series: [{name: '% Sucesso', data: newSeries}]
+        });
+      })
+  }, [])
+
   const options = {
     plotOptions: {
       bar: {
@@ -9,21 +42,10 @@ function BarChart() {
     },
   };
 
-  const mockData = {
-    labels: {
-      categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    },
-    series: [
-      {
-        name: "% Sucesso",
-        data: [43.6, 67.1, 67.7, 45.6, 71.1]
-      }
-    ]
-  };
   return (
     <Chart
-      options={{ ...options, xaxis: mockData.labels }}
-      series={mockData.series}
+      options={{ ...options, xaxis: dataChart.labels }}
+      series={dataChart.series}
       type="bar"
       height="240" />
   );
